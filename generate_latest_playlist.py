1cf1693a1ee6bb50558fea1967df78ac2726fdf1
Maciej Jonasz <maciej.jonasz@kontomatik.com>
2019-06-05 14:54:52 +0200

    https for scoresaber

--- generate_latest_playlist.py
+++ generate_latest_playlist.py
@@ -5,7 +5,7 @@ import base64
 import sys
 from datetime import date
 
-top_pp_response = requests.get("http://scoresaber.com/api.php?function=get-leaderboards&cat=3&page=1&limit=300&ranked=1")
+top_pp_response = requests.get("https://scoresaber.com/api.php?function=get-leaderboards&cat=3&page=1&limit=300&ranked=1")
 bsaver_data_response = requests.get("https://raw.githubusercontent.com/andruzzzhka/BeatSaberScrappedData/master/beatSaverScrappedData.json")
 
 top_pp = json.loads(top_pp_response.text)
